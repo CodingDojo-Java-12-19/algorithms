7bf213bfbdd4585461d1540d1e5b7e41e0ebdbe2
Daniel Duane Beatty Jr <tandd2015@outlook.com>
2019-11-27 12:24:38 -0500

    another morinin

--- trees/trees.js
+++ trees/trees.js
@@ -43,6 +43,36 @@ class Tree {
         }
         return this;
     };
+
+    add2(value) { 
+        const node = new BNode(value);
+        if(this.root === null) {
+            this.root = node;
+        } else {
+            this.root.add2(node);
+        }
+    }
+// BST: Min
+// Create a min() method on the BST class that returns the smallest value found in the BST”
+// Excerpt From: Martin Puryear. “Algorithm Challenges: E-book for Dojo Students.” Apple Books. 
+    min() {
+        if(this.root === null) {
+            return null;
+        } else {
+            return this.root.min();
+        }
+    }
+    contains(val) {
+        if(this.root === null) {
+            return null;
+        } else {
+            return this.root.contains(val);
+        }
+    }
+    // BST: Contains
+    // Create a contains(val) method on BST that returns whether the tree contains a given value.
+    // Take advantage of the BST structure to make this a much more rapid operation than SList.contains() would be.”
+    // Excerpt From: Martin Puryear. “Algorithm Challenges: E-book for Dojo Students.” Apple Books. 
 }
 
 class BNode {
@@ -50,14 +80,63 @@ class BNode {
         this.value = Bnodin;
         this.left = null;
         this.right = null;
-    };
+    }
+    add2(node) {
+        if(node.value >= this.value){
+            if(this.right === null){
+                this.right = node;
+            } else {
+                this.right.add2(node);
+            }
+        } else {
+            if(this.left === null){
+                this.left = node;
+            } else {
+                this.left.add2(node);
+            }
+        }
+    }
+    min() {
+        if(this.left !== null) {
+            return this.left.min();
+        } else {
+            return this.value;
+        }
+    }
+    contains(val) {
+        if(this.value === val){
+            return true;
+        }
+        if(this.value > val) {
+            if(this.left !== null){
+                return this.left.contains(val);
+            }
+        } else {
+            if(this.right !== null) {
+                return this.right.contains(val);
+            }
+        }
+        return false;
+    }
 }
 
 const BTree = new Tree();
+const B2Tree = new Tree();
 
-BTree.add(10);
-BTree.add(15);
-BTree.add(5);
-BTree.add(20);
+B2Tree.add(10);
+B2Tree.add(15);
+B2Tree.add(5);
+B2Tree.add(20);
+B2Tree.add(3);
 
-console.log(BTree);
+console.log(B2Tree.min());
+console.log(B2Tree.contains(5));
+console.log(B2Tree.contains(20));
+console.log(B2Tree.contains(25));
+
+// BTree.add(10);
+// BTree.add(15);
+// BTree.add(5);
+// BTree.add(20);
+
+console.log(B2Tree);
